a0eb82cadfe38dd82c1d0d59b116364b9f806ceb
sergio-alv <alvarezpeirosergio@gmail.com>
2020-11-01 22:11:16 +0100

    Tests against remote services failures and network failures

--- src/test/java/translator/web/ws/TranslatorEndpointTest.java
+++ src/test/java/translator/web/ws/TranslatorEndpointTest.java
@@ -9,6 +9,8 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.ClassUtils;
+import org.springframework.ws.client.WebServiceIOException;
+import org.springframework.ws.client.WebServiceTransportException;
 import org.springframework.ws.client.core.WebServiceTemplate;
 
 import translator.Application;
@@ -44,6 +46,36 @@ public class TranslatorEndpointTest {
     request.setLangTo("es");
     request.setText("This is a test of translation service");
     Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost:"
+            + port + "/ws", request);
+    assertNotNull(response);
+    assertThat(response, instanceOf(GetTranslationResponse.class));
+    GetTranslationResponse translation = (GetTranslationResponse) response;
+    assertThat(translation.getTranslation(), is("I don't know how to translate from en to es the text 'This is a test of translation service'"));
+  }
+
+  // Test against remote services failures
+  @Test(expected = WebServiceTransportException.class)
+  public void testRemoteServiceFailure() {
+    GetTranslationRequest request = new GetTranslationRequest();
+    request.setLangFrom("en");
+    request.setLangTo("es");
+    request.setText("This is a test of translation service");
+    Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost:"
+            + port + "/invalid", request);
+    assertNotNull(response);
+    assertThat(response, instanceOf(GetTranslationResponse.class));
+    GetTranslationResponse translation = (GetTranslationResponse) response;
+    assertThat(translation.getTranslation(), is("I don't know how to translate from en to es the text 'This is a test of translation service'"));
+  }
+
+  // Test against network failures
+  @Test(expected = WebServiceIOException.class)
+  public void testRemoteServiceDown() {
+    GetTranslationRequest request = new GetTranslationRequest();
+    request.setLangFrom("en");
+    request.setLangTo("es");
+    request.setText("This is a test of translation service");
+    Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://somehost:"
             + port + "/ws", request);
     assertNotNull(response);
     assertThat(response, instanceOf(GetTranslationResponse.class));
